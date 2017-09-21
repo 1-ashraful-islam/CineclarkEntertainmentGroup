a3b3aae6d84369d3b87a40c1024c5c324aa008d0
mislam <mislam@cse.unl.edu>
2017-09-20 23:30:56 -0500

    adding the default classes for data containers

---
+++ src/org/cineclark/datacontainers/Customer.java
@@ -0,0 +1,52 @@
+package org.cineclark.datacontainers;
+
+public class Customer {
+
+	private String customerCode;
+	private Person contact;
+	private String name;
+	private Address address;
+	
+	public Customer(String customerCode, Person contact, String name, Address address) {
+		super();
+		this.customerCode = customerCode;
+		this.contact = contact;
+		this.name = name;
+		this.address = address;
+	}
+
+	public String getCustomerCode() {
+		return customerCode;
+	}
+
+	public void setCustomerCode(String customerCode) {
+		this.customerCode = customerCode;
+	}
+
+	public Person getContact() {
+		return contact;
+	}
+
+	public void setContact(Person contact) {
+		this.contact = contact;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+	
+}
